5b6c92424d4d28ee4e266a58bef0465805ec04d6
Jose Lopez <jose.m.lopez747@gmail.com>
2022-02-07 15:43:49 -0600

    finished while loop exercise

---
+++ js/while.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// var number = 1;
+// while (number < 65536) {
+//     number = number * 2
+//     console.log(number);
+// }
+
+
+
+// var allCones = Math.floor(Math.random () * 50) + 50;
+//
+// do {
+//     var conesBought = Math.floor(Math.random() * 5) +1;
+//     allCones = allCones - conesBought
+//     if ( allCones - conesBought >= 0) {
+//         console.log(allCones + " cones sold.")
+//     } else {
+//         console.log("I don't have enough cones!")
+//     }
+// } while (allCones > 0)
+// console.log("Yay! You sold all your cones!")
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+do {
+    var conesBeingBought = Math.floor(Math.random() * 5) + 1;
+    if (conesBeingBought > allCones) {
+        console.log("Cannot sell you " + conesBeingBought + " I only have " + allCones);
+    } else {
+        console.log(conesBeingBought + " cones sold...");
+    }
+} while (allCones > 0);
+console.log("Yay! I sold them all!");

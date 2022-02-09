f73d5193ef2d4fbb3009cec7dfe1e37f394d1e59
Jose Lopez <jose.m.lopez747@gmail.com>
2022-02-09 16:58:34 -0600

    completed planets array and string exercises

---
+++ js/planets-string.js
@@ -0,0 +1,40 @@
+(function(){
+    "use strict";
+
+    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray;
+
+    /**
+     * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */
+    console.log(planetsString);
+    var newplanetsString = planetsString.replaceAll('|', ' ');
+    console.log(newplanetsString);
+    var planetsArray = newplanetsString.split(' ');
+    console.log(planetsArray);
+
+    var planetsArray =  planetsString.split( '|');
+    console.log(planetsArray);
+
+
+
+    /**
+     * TODO:
+     * Create a string with <br> tags between each planet. console.log() your
+     * results. Why might this be useful?
+     *
+     * BONUS:
+     * Create another string that would display your planets in an undordered
+     * list. You will need an opening AND closing <ul> tags around the entire
+     * string, and <li> tags around each planet.
+     */
+    var planetsBacktoString = planetsArray.join('<br>');
+    console.log(planetsBacktoString)
+    console.log(planetsArray.join("<br>"));
+
+    var output = '<ul><li>' + planetsArray.join('</li><li>') + '</li></li>';
+    document.write(output);
+})();
